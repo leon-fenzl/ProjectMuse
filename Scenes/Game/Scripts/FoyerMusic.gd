5abf2befe25f9c6a2e28e92230c2687308e760e6
Turith1 <162712980+Turith1@users.noreply.github.com>
2024-10-19 23:48:36 -0300

    BG sound

---
+++ Scenes/Game/Scripts/FoyerMusic.gd
@@ -0,0 +1,29 @@
+extends AudioStreamPlayer3D
+
+@onready var audio = $"."
+
+var chosenInt: int = 0
+var restrictedArea = false
+
+var array = ["res://Sounds/BGSounds/1 Game Play - Magnus Games Audio.wav","res://Sounds/BGSounds/2 Game Play - Magnus Games Audio.wav","res://Sounds/BGSounds/3 Game Play - Magnus Games Audio.wav", "res://Sounds/BGSounds/Fase 1 - Primeiro Quadro - 2.wav"]
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	rng.randomize()
+	chosenInt = rng.randi_range(0, 2)
+	audio.stream = load(array[chosenInt])
+	audio.play()
+
+func _physics_process(delta: float) -> void:
+	if restrictedArea == true:
+		if !audio.playing:
+			audio.stream = load(array[3])
+			audio.play()
+	else:
+		if !audio.playing:
+			var x = chosenInt
+			while chosenInt == x:
+				rng.randomize()
+				chosenInt = rng.randi_range(0, 2)
+			audio.stream = load(array[chosenInt])
+			audio.play()
