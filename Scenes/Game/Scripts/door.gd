a67664bcd8c8a89ec4aeac1327cd40a08f89a77a
VictorStriederGib <victorstriedereducaverso@gmail.com>
2024-10-19 21:31:00 -0300

    Lerp da porta
    Só precisa adicionar o script door a um static body tendo um node marker3D como referência de ponto final

---
+++ Scenes/Game/Scripts/door.gd
@@ -0,0 +1,32 @@
+extends StaticBody3D
+
+var markerPosition = Vector3(0,0,0)
+@export var velocidade : float = 0.01
+var time : float = 0
+##@export var 
+##@export var targetPosition : Vector3()
+
+func _ready() -> void:
+	pass # Replace with function body.
+	
+	##get the final position
+	markerPosition = get_node("Marker3D")
+	Open_Door()
+
+func Open_Door():
+	##Start opennig the door
+	OpeningDoor()
+	pass
+	
+func OpeningDoor():
+	if time <= 1:
+		time += get_process_delta_time() * velocidade
+		position = position.lerp(markerPosition.position,time)
+		await get_tree().create_timer(0,0001).timeout
+		OpeningDoor()
+		return
+ 
+
+
+##physiscs process
+##Unique name
