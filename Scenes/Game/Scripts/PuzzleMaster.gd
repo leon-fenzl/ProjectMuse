d03d13a2d18fba253e63cce54d6d3a9c0f4e71dc
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-10-20 13:10:01 -0300

    Update Game scene PRE BUILD

--- Scenes/Game/Scripts/PuzzleMaster.gd
+++ Scenes/Game/Scripts/PuzzleMaster.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 @export var locks:Array[Area3D]
 @export var solution:Array[int]
+
 var isChecking := false
 var index :int=0 
 var onPlace := false
@@ -13,27 +14,6 @@ func CheckSolution():
 			Completed()
 		else:
 			return
-	#if locks[index].check == true:
-		#if index <= locks.size()-1:
-			#onPlace = true'
-			#Completed()
-			#index += 1
-		#if index >= locks.size()-1:
-			#index += 1
-	#else:
-		#index = 0
-		#NotCompleted()
-	#if index <= locks.size()-1:
-		#index += 1
-	#if index > locks.size()-1:
-		#index = 0
-	#if locks[index].check == true:
-		#completed = true
-		#Completed()
-		#print(completed)
-	#else:
-		#index = 0
-		#NotCompleted()
 static func Completed():
 	return true
 static  func NotCompleted():
--- Scenes/Game/Scripts/PuzzlePiece.gd
+++ Scenes/Game/Scripts/PuzzlePiece.gd
@@ -1,27 +1,26 @@
 extends CharacterBody3D
 enum PIECETYPE{NOTSOLID,SOLID}
 @export var piecetype :PIECETYPE = PIECETYPE.NOTSOLID
-@export var shape : Shape3D
+#@export var shape : Shape3D
 @export var keyValue : int
 @export var cuts : float
 @onready var colShape := $CollisionShape3D
 @export var img : Texture
 @onready var sprite := $Sprite3D
 func _ready() -> void:
-	SetShapeType()
+	#SetShapeType()
 	SetSprite()
 func FollowMouse(newPosition):
 	position.x = newPosition.x
 	position.y = newPosition.y
-func SetShapeType():
-	match piecetype:
-		PIECETYPE.NOTSOLID:
-			return
-		PIECETYPE.SOLID:
-			colShape.shape = shape
+#func SetShapeType():
+	#match piecetype:
+		#PIECETYPE.NOTSOLID:
+			#return
+		#PIECETYPE.SOLID:
+			#colShape.shape = shape
 func SetSprite():
 	sprite.hframes = cuts
 	sprite.vframes = cuts
 	sprite.frame = keyValue
 	sprite.texture = img
-	
