c56986f83750ea6e8a74bb5c9bcaa4c6742e5110
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-12-12 23:24:15 -0300

    Some tryouts

--- Scripts/piece.gd
+++ Scripts/piece.gd
@@ -11,32 +11,30 @@ var sprite : Sprite3D = Sprite3D.new()
 @export var adStream : AudioStreamPlayer
 @onready var input_dirs := Vector3.ZERO
 @onready var moveDirection := Vector3.ZERO
-#var direction := Vector2.ZERO
-#var fps : float
-#var underay : bool = false
-@export var texture :Texture2D:
-	get():
-		return texture
-	set(value):
-		texture = value
-		Update_Texture(value)
-@export var split :Vector2:
-	get():
-		return split
-	set(value):
-		if value != split:
-			split = value
-			Update_HFrames(value.x)
-			Update_VFrames(value.y)
-@export var frame:int:
-	get():
-		return frame
-	set(value):
-		if value != frame:
-			frame = value
-			Update_Frames(value)
+#@export var texture :Texture2D:
+	#get():
+		#return texture
+	#set(value):
+		#texture = value
+		#Update_Texture(value)
+#@export var split :Vector2:
+	#get():
+		#return split
+	#set(value):
+		#if value != split:
+			#split = value
+			#Update_HFrames(value.x)
+			#Update_VFrames(value.y)
+#@export var frame:int:
+	#get():
+		#return frame
+	#set(value):
+		#if value != frame:
+			#frame = value
+			#Update_Frames(value)
+#func _init() -> void:
+	#Construct_Object_Ingame()
 func _ready() -> void:
-	Construct_Object_Ingame()
 	match piecetype:
 		PIECETYPE.NOTSOLID:
 			$CollisionShape3D.disabled = true
@@ -56,18 +54,18 @@ func _physics_process(delta: float) -> void:
 				MOVETYPE.DONTMOVE:
 					pass
 	move_and_slide()
-func Update_Texture(value):
-	sprite.texture = value
-func Update_HFrames(value):
-	sprite.hframes = value
-func Update_VFrames(value):
-	sprite.vframes = value
-func Update_Frames(value):
-	sprite.frame = value
-func DrawCol(value):
-	$".".add_child(value)
-func Construct_Object_Ingame():
-	$".".add_child(sprite)
+#func Update_Texture(value):
+	#sprite.texture = value
+#func Update_HFrames(value):
+	#sprite.hframes = value
+#func Update_VFrames(value):
+	#sprite.vframes = value
+#func Update_Frames(value):
+	#sprite.frame = value
+#func DrawCol(value):
+	#$".".add_child(value)
+#func Construct_Object_Ingame():
+	#$".".add_child(sprite)
 func Move_Piece(DELTA:float):
 	input_dirs.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_dirs.y = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
